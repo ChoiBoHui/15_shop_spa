4baccbd499d5c3c2d7b32515fb63a92902ee3464
choibohui <123dfga@naver.com>
2022-09-26 16:32:48 +0900

    20220926

---
+++ src/data/data.js
@@ -0,0 +1,22 @@
+const company = {
+    name: '나의 쇼핑몰 이야기',
+    address: '부산광역시 진구 ㅇㅇㅇ로 105-453',
+    tel: '051-000-0000',
+    fax: '051-000-1000',
+    ceo: 'raiman Kin'
+}
+
+const profile = {
+    slogan: '어쨋든 많이 팔자.'
+}
+
+const content = [
+    { id: 11, con: '슬로건 01', des: '설명글 들어가는 칸 01' },
+    { id: 21, con: '슬로건 02', des: '설명글 들어가는 칸 02' },
+    { id: 31, con: '슬로건 03', des: '설명글 들어가는 칸 03' },
+    { id: 41, con: '슬로건 04', des: '설명글 들어가는 칸 04' },
+    { id: 51, con: '슬로건 05', des: '설명글 들어가는 칸 05' },
+    { id: 61, con: '슬로건 06', des: '설명글 들어가는 칸 06' }
+]
+
+export { company, profile, content }
